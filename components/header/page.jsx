3d5917350a8ba518fa23ca36aadca7316e3a6ca4
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2025-05-05 17:50:03 +0300

    Fix image source path for logo in Header component

--- components/header/page.jsx
+++ components/header/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header>
-      <Image width={80} height={80} src={"/img/todologo.svg"} alt="görev kutusu logosu" />
+      <Image width={80} height={80} src={"/img/todoLogo.svg"} alt="görev kutusu logosu" />
       <h1>Görev Kutusu</h1>
     </header>
   )
